e9f9570ed1f31086a292335f089ef26e4792a650
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2019-12-31 11:21:43 +0100

    added demo playing

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -4,17 +4,20 @@ import { SignalChart } from '../common/SignalChart';
 import { Dsp } from '../common/Dsp';
 import { LowPassFilter } from '../services/lowpassfilter';
 import { StackedBuffer } from '../services/stackedbuffer';
-import { SignalApi } from '../services/api';
+import { SignalApi, DemoApi } from '../services/api';
 
 export class Home extends Component {
   //static displayName = Home.name;
+    static bufferSize = 10;
 
     constructor(props) {
         super();
-        this.inputBuffer = new StackedBuffer(10, this.onInputBufferPop.bind(this));
-        this.lowPassBuffer = new StackedBuffer(10, this.onLowPassBufferPop.bind(this));
+        this.inputBuffer = new StackedBuffer(Home.bufferSize, this.onInputBufferPop.bind(this));
+        this.demoBuffer = new StackedBuffer(Home.bufferSize, this.onDemoInputBufferPop.bind(this));
+        this.lowPassBuffer = new StackedBuffer(Home.bufferSize, this.onLowPassBufferPop.bind(this));
         this.lowPassFilter = new LowPassFilter(64, 24, this.lowPassBuffer.push.bind(this.lowPassBuffer));
         this.signalApi = new SignalApi([this.inputBuffer.push.bind(this.inputBuffer), this.lowPassFilter.push.bind(this.lowPassFilter)]);
+        this.demoApi = new DemoApi([this.demoBuffer.push.bind(this.demoBuffer), this.lowPassFilter.push.bind(this.lowPassFilter)])
 
         // references
         this.recordButton = React.createRef();
@@ -26,24 +29,31 @@ export class Home extends Component {
 
     onInputBufferPop(values) {
         if (this.pauseButton.current.paused()) { return; }
+        this.demoApi.disconnect();
         this.rawSignalChart.current.process(values);
         this.recordButton.current.process(values);
+    }
+
+    onDemoInputBufferPop(values) {
+        if (this.pauseButton.current.paused()) { return; }
+        this.rawSignalChart.current.process(values);
     }
 
     onLowPassBufferPop(values) {
         if (this.pauseButton.current.paused()) { return; }
         this.rawSignalChart.current.process(values);
-        //this.lowPassSignalChart.current.process(values);
         //this.dsp.process(values);
     }
 
     componentWillMount() {
         this.signalApi.connect();
+        this.demoApi.connect();
     }
 
     componentWillUnmount() {
         this.signalApi.disconnect();
         this.inputBuffer.clear();
+        this.demoBuffer.clear();
     }
 
     sliderChange(evt) {
@@ -57,7 +67,7 @@ export class Home extends Component {
             <div className="btn-group">
                 <RecordButton ref={this.recordButton} />
                 <PauseButton ref={this.pauseButton} />
-                <input type="range" class="custom-range" min="0" max="255" id="customRange2" onChange={this.sliderChange} />
+                <input type="range" className="custom-range" min="0" max="255" id="customRange2" onChange={this.sliderChange} />
             </div>
             <SignalChart ref={this.rawSignalChart} name="rawSignalChart" title="Raw Signal" expiration={30} />
             <Dsp ref={this.dsp} />
--- ClientApp/src/services/api.js
+++ ClientApp/src/services/api.js
@@ -1,9 +1,18 @@
 ﻿import * as signalR from "@microsoft/signalr";
+import moment from 'moment';
 
 export class Api {
 
     async getNewRecording() {
         return await (await fetch('/api/Recording/0')).json();
+    }
+
+    async getRecordings() {
+       return await (await fetch(`/api/Recording`)).json();
+    }
+
+    async loadRecording(rid) {
+        return await (await fetch(`/api/Recording/${rid}`)).json();
     }
 
     async saveRecording(recordingId, metrics) {
@@ -46,5 +55,56 @@ export class SignalApi {
 
     disconnect() {
         this.hubConnection.stop().then(() => console.log('Signal Connection closed!'));
+    }
+}
+
+export class DemoApi {
+
+    active = false;
+    demos = null;
+
+    constructor(onSignalCallbacks) {
+        this.onSignalCallbacks = onSignalCallbacks;
+    }
+
+    async sleep(milliseconds) {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
+    async connect() {
+        this.active = true;
+        let api = new Api();
+        let rs = await api.getRecordings();
+        let dr = rs.filter(o => o.name === 'demo');
+        if (dr && dr.length > 0) {
+            this.demos = (await api.loadRecording(dr[0].id)).recordingMetrics;
+        }
+        
+        while (this.active && this.demos) {
+            if (this.demos && this.demos.length > 0) {
+                for (let i = 0; i < this.demos.length; i++) {
+                    if (this.onSignalCallbacks) {
+                        let o = Object.assign({}, this.demos[i]);
+                        o.timestamp = new Date();
+                        if (Array.isArray(this.onSignalCallbacks)) {
+                            this.onSignalCallbacks.forEach(f => f(o));
+                        }
+                        else {
+                            this.onSignalCallbacks(this.demos[i]);
+                        }
+                        // sleep until next call
+                        let ms = i < this.demos.length - 1 ? moment(this.demos[i + 1].timestamp).diff(moment(this.demos[i].timestamp)).valueOf() : 40;
+                        await this.sleep(ms);
+                        if (!this.active) {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    disconnect() {
+        this.active = false;
     }
 }
