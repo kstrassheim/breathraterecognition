6aae8672576ad54ca55fb301fe189e3aebdaa718
Konstantin Strassheim <konstantin.strassheim@googlemail.com>
2020-02-09 00:31:33 +0000

    Merged PR 6: added hosting from assebly dir

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +22,7 @@ namespace BreathRateRecognition
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).UseStartup<Startup>();
                 });
     }
 }
